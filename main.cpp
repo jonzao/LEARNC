c782b047c3f8b2d6b455bff512f1e319a7e3bb5f
jonzao <237889073@qq.com>
2020-09-20 14:10:45 +0800

    1.1

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class A
+{
+public:
+    A();
+    ~A() noexcept(false)
+    {
+        throw string("error");
+    }
+};
+
+void test()
+{
+
+    try
+    {
+        A a();
+    }
+    catch (string e)
+    {
+
+        cout << "object  A  is except : " + e << endl;
+    }
+}
+int main()
+{
+    test();
+    return 0;
+}
